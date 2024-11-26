98134113edfc50a1a4118b99fa4b722ca5211f42
Anil Kumar Yadav <anilkumar.yadav@cognologix.com>
2024-11-26 14:51:08 +0530

    adding inside try catch user controller

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -7,22 +7,26 @@ const generateToken = (id) => {
 
 const loginController = async (req, res) => {
   const { number, password } = req.body;
-  const user = await userModel.findOne({ number });
-  if (!user) {
-    return res.json({ message: "User not found, Please register first!" });
-  }
+  try {
+    const user = await userModel.findOne({ number });
+    if (!user) {
+      return res.json({ message: "User not found, Please register first!" });
+    }
 
-  if (password != user.password) {
-    return res.json({ message: "Invalid Phone or Password" });
-  }
+    if (password != user.password) {
+      return res.json({ message: "Invalid Phone or Password" });
+    }
 
-  res.json({
-    _id: user._id,
-    name: user.name,
-    number: user.number,
-    token: generateToken(user._id),
-    message: "Login successfully",
-  });
+    res.json({
+      _id: user._id,
+      name: user.name,
+      number: user.number,
+      token: generateToken(user._id),
+      message: "Login successfully",
+    });
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
 
 const signupController = async (req, res) => {
@@ -30,25 +34,35 @@ const signupController = async (req, res) => {
   if (number === "" || password === "" || username === "") {
     return res.json({ message: "Please fill all field", success: false });
   }
-  if(number.length !== 10){
+  if (number.length !== 10) {
     return res.json({ message: "Please enter a valid number", success: false });
   }
-  const existUser = await userModel.findOne({ number });
-  if (existUser) {
-    return res.json({ message: "User already exist, Please login!", success: false });
+  try {
+    const existUser = await userModel.findOne({ number });
+    if (existUser) {
+      return res.json({
+        message: "User already exist, Please login!",
+        success: false,
+      });
+    }
+    const collection = await userModel({ number, password, name: username });
+    const result = await collection.save();
+    res.send({ message: "Register Successfully", success: true });
+  } catch (error) {
+    console.log("Error", error);
   }
-  const collection = await userModel({ number, password, name: username });
-  const result = await collection.save();
-  res.send({ message: "Register Successfully", success: true });
 };
 
 const profileController = async (req, res) => {
   if (!req.user) {
     return res.json({ message: "Unauthorized user" });
   }
-
-  const userDoc = await userModel.findById(req.user._id);
-  res.send({ userDetail: userDoc });
+  try {
+    const userDoc = await userModel.findById(req.user._id);
+    res.send({ userDetail: userDoc });
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
 
 module.exports = { loginController, signupController, profileController };
